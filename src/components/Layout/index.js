ad2fb77f112e27eb70dc17e237b594c66706bf5b
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-02-12 22:37:02 -0800

    Add routes

---
+++ src/components/Layout/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import styles from './index.css';
+
+const Layout = ({ children }) => <div className={styles.layoutWrapper}></div>;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return <div>Hello world!</div>;
-  }
-}
+import LoginPage from './routes/Login';
+import SignupPage from './routes/Signup';
+import ForumPage from './routes/Forum';
+
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage token={token} />
+        </Route>
+        <Route path="/signup">
+          <SignupPage token={token} />
+        </Route>
+        <Route path="/">
+          <ForumPage token={token} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
